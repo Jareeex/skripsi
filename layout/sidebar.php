7ba7b324f06b8eb673a173b36552100ae2baf4be
Jareeex <baswarafajar@gmail.com>
2025-04-09 14:53:33 +0700

    Add files via upload

---
+++ layout/sidebar.php
@@ -0,0 +1,65 @@
+<div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <div class="logo">
+                <a href="./">
+                    <img src="/skripsi3/assets/images/andatama.jpg" alt="Logo" class="sidebar-logo"> 
+                    
+                </a>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title">Menu</li>
+
+                <li class="sidebar-item">
+                    <a href="./" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                        <span>Data</span>
+                    </a>
+                    <ul class="submenu">
+                        <li class="submenu-item">
+                            <a href="alternatif.php">Alternatif</a>
+                        </li>                        
+                        <li class="submenu-item">
+                            <a href="bobot.php"> Kriteria</a>
+                        </li>
+                        <li class="submenu-item">
+                            <a href="cetakpdf.php">Cetak PDF</a>
+                        </li>
+                        </ul>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="matrik.php" class='sidebar-link'>
+                        <i class="bi bi-pentagon-fill"></i>
+                        <span>Matrik</span>
+                    </a>
+                </li>
+                
+                <li class="sidebar-item">
+                    <a href="preferensi.php" class='sidebar-link'>
+                        <i class="bi bi-bar-chart-fill"></i>
+                        <span>Nilai Preferensi</span>
+                    </a>
+                </li>
+
+                  <li class="sidebar-item">
+                    <a href="logout.php" class='sidebar-link'>
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
